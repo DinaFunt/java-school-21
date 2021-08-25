b7c895e6c4f57cf70fab33e275a312da2eb35bea
DinaFunt <dina.funt@gmail.com>
2021-08-26 00:20:13 +0300

    black friday counting added

--- src/main/java/hw/lab6/homework/BlackFridayService.java
+++ src/main/java/hw/lab6/homework/BlackFridayService.java
@@ -1,14 +1,35 @@
 package hw.lab6.homework;
 
-/**
- * @author Evgeny Borisov
- */
+
+import hw.lab6.employees.Employee;
+import hw.lab6.employees.Seniority;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class BlackFridayService {
 
-    public void printBlackFridayPerYearSorted(int startYear, int endYear) {
+    public static void printBlackFridayPerYearSorted(int startYear, int endYear) {
 
-        //todo do that!!!!!
+        LocalDate startDate = LocalDate.of(startYear, 1, 13);
+        LocalDate endDate = LocalDate.of(endYear, 1, 13);
+        long monthCount = ChronoUnit.MONTHS.between(startDate, endDate);
 
+
+        Map<Integer, Long> results = Stream.iterate(startDate, date -> date.plusMonths(1))
+                .limit(monthCount)
+                .filter(curDate -> curDate.getDayOfWeek() == DayOfWeek.FRIDAY)
+                .collect(Collectors.groupingBy(LocalDate::getYear, Collectors.counting()));
+
+        results.entrySet().stream().
+                sorted(Comparator.comparing(Map.Entry<Integer, Long>::getValue).
+                        thenComparing(Map.Entry::getKey).reversed())
+                .forEach(System.out::println);
 //        IntStream.range(startYear,endYear)
 
 //        Stream.iterate(startDate,date ->date.plusDay() )
---
+++ src/main/java/hw/lab6/homework/Main.java
@@ -0,0 +1,9 @@
+package hw.lab6.homework;
+
+public class Main {
+    public static void main(String[] args) {
+        int startYear = 1999;
+        int endYear = 2021;
+        BlackFridayService.printBlackFridayPerYearSorted(startYear, endYear);
+    }
+}
